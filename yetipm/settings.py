b3d82e32a83a50af8707b432a56f35c90fcd145f
vishaldhakal <vishaldhakal96@gmail.com>
2025-01-14 19:41:27 +0545

    sdcsdc

--- yetipm/settings.py
+++ yetipm/settings.py
@@ -210,29 +210,3 @@ TINYMCE_DEFAULT_CONFIG = {
 
 
 SUMMERNOTE_THEME = 'bs4'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
-
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
