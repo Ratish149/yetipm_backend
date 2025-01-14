0f46e6bdf81dc20e0e4e0a12b473983bfecd64f6
Ratish149 <ratish.shakya149@gmail.com>
2025-01-14 15:41:56 +0545

    dfdf

--- blog/models.py
+++ blog/models.py
@@ -14,12 +14,18 @@ class SlugMixin:
             slug = base_slug  # Just use the base slug if it exists
         self.slug = slug
 
-        
+class Tag(models.Model):
+    name = models.CharField(max_length=220)
+
+    def __str__(self):
+        return self.name
+
 class Blog(SlugMixin, models.Model):
     title = models.CharField(max_length=220)
     content = models.TextField()
     image = models.ImageField(upload_to='blog_images/',null=True,blank=True)
     slug = models.SlugField(unique=True, blank=True,max_length=220)
+    tags = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
