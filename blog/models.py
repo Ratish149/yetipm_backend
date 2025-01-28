d17e397606a39f36e4d1783c1bbc6af3cd98b72d
Ratish149 <ratish.shakya149@gmail.com>
2025-01-28 13:36:38 +0545

    sadad

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django_summernote.fields import SummernoteTextField
-from ckeditor_uploader.fields import RichTextUploadingField
+
 from tinymce import models as tinymce_models
+from django.utils.text import slugify
+
+
+class SlugMixin:
+    def generate_unique_slug(self):
+        base_slug = slugify(self.title)
+        slug = base_slug
+
+        model = self.__class__
+        while model.objects.filter(slug=slug).exclude(id=self.id).exists():
+            slug = base_slug
+
+        self.slug = slug
+
+    def save(self, *args, **kwargs):
+        self.generate_unique_slug()
+        super().save(*args, **kwargs)
 
 
 class Author(models.Model):
     name = models.CharField(max_length=200) 
     role = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
-    picture = models.FileField()
-    about = models.TextField(blank=True)
+    picture = models.FileField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -19,7 +34,7 @@ class Author(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=200, primary_key=True)
-    category_image = models.FileField(blank=True)
+    category_image = models.FileField(blank=True,null=True)
 
     def __str__(self):
         return self.category_name
@@ -31,21 +46,19 @@ class Tag(models.Model):
         return self.tag_name
 
 
-class Post(models.Model):
+class Post(SlugMixin,models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    slug = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=300, unique=True,null=True,blank=True)
     title = models.CharField(max_length=500)
     blog_duration_to_read = models.CharField(max_length=100,blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    thumbnail_image = models.FileField()
-    thumbnail_image_alt_description = models.CharField(max_length=300)
+    thumbnail_image = models.FileField(null=True, blank=True)
+    thumbnail_image_alt_description = models.CharField(max_length=300,null=True,blank=True)
     blog_content = tinymce_models.HTMLField(blank=True)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag,blank=True)
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
-    meta_title = models.CharField(max_length=200)
-    meta_description = models.TextField()
-    meta_keywords = models.TextField(blank=True)
+
 
     class Meta:
         ordering = ['-created_at']
