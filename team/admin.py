43bca727c26cfdc6fe6e970ae18238bc7a148d71
Ratish149 <ratish.shakya149@gmail.com>
2025-01-29 13:21:48 +0545

    dsds

--- team/admin.py
+++ team/admin.py
@@ -6,7 +6,7 @@ from tinymce.widgets import TinyMCE
 
 # Register your models here.
 class OurTeamAdmin(ModelAdmin):
-    list_display = ('name', 'role', 'email', 'description')  # Fields to display in the list view
+    list_display = ('name', 'role', 'email', 'created_at')  # Fields to display in the list view
     search_fields = ('name', 'role')  # Fields to search in the admin
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()},
