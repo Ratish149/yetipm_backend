465246afeb94c6a9f2953fd1f207d158fcfc343f
Ratish149 <ratish.shakya149@gmail.com>
2025-01-29 12:45:11 +0545

    sdsd

--- team/admin.py
+++ team/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import OurTeam
 from unfold.admin import ModelAdmin
+from django.db import models
+from tinymce.widgets import TinyMCE
+
 # Register your models here.
 class OurTeamAdmin(ModelAdmin):
-    list_display = ('name', 'role', 'email')  # Fields to display in the list view
+    list_display = ('name', 'role', 'email', 'description')  # Fields to display in the list view
     search_fields = ('name', 'role')  # Fields to search in the admin
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
 
 admin.site.register(OurTeam, OurTeamAdmin)
