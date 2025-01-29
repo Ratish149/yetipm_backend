ace14820f3b9da49d15a6fb75d3c6007427d06d2
Ratish149 <ratish.shakya149@gmail.com>
2025-01-29 12:05:20 +0545

    fdf

--- team/admin.py
+++ team/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import OurTeam
+from unfold.admin import ModelAdmin
 # Register your models here.
-class OurTeamAdmin(admin.ModelAdmin):
+class OurTeamAdmin(ModelAdmin):
     list_display = ('name', 'role', 'email')  # Fields to display in the list view
     search_fields = ('name', 'role')  # Fields to search in the admin
 
