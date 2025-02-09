70a7fc0abd5e40535d7419cd15975bfe2b212358
Ratish149 <ratish.shakya149@gmail.com>
2025-02-09 14:42:37 +0545

    sds

--- projects/views.py
+++ projects/views.py
@@ -72,7 +72,7 @@ class ProjectFilter(django_filters.FilterSet):
     beds = django_filters.NumberFilter(field_name="bedrooms", lookup_expr='gte')
     baths = django_filters.NumberFilter(field_name="bathrooms", lookup_expr='gte')
     property_type = django_filters.CharFilter(field_name="project_type", lookup_expr='iexact')
-    city = django_filters.CharFilter(field_name="city__name", lookup_expr='icontains')
+    city = django_filters.CharFilter(field_name="city__id", lookup_expr='iexact')
     min_area_square_footage = django_filters.NumberFilter(field_name="area_square_footage", lookup_expr='gte')
     max_area_square_footage = django_filters.NumberFilter(field_name="area_square_footage", lookup_expr='lte')
     features = django_filters.ModelMultipleChoiceFilter(queryset=Features.objects.all(), field_name='features')
