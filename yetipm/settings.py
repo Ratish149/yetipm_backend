99cc00f928deb5aff70c2947927d7323bcf22906
Ratish149 <ratish.shakya149@gmail.com>
2025-02-18 17:06:11 +0545

    asd

--- yetipm/settings.py
+++ yetipm/settings.py
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'yetipm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'yeti_pm',
@@ -106,7 +106,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     } 
-}"""
+}
  
 DATA_UPLOAD_MAX_MEMORY_SIZE = None
 
